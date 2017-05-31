ea56d6ddf96c6126829c0bb38413aaf9cc51acb4
Tim Holwerda <tim@highvibes.io>
2017-05-31 17:24:00 +0200

    Updated port resolution

--- index.js
+++ index.js
@@ -2,11 +2,12 @@ const express = require('express');
 const router = require('./routes.js');
 
 const app = express()
+const port = process.env.PORT || 3000;
  
 app.set('view engine', 'pug')
 app.use( router ); 
 app.use( express.static('public') );
 
-app.listen(3000, function () {
-  console.log('PWA listening on port 3000!')
+app.listen(port, function () {
+  console.log(`PWA listening on port ${port}!`)
 })
