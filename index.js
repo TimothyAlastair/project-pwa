1dd29a5b125df69e72f7e68204f501b6ce38fae9
Tim Holwerda <tim@highvibes.io>
2017-05-31 16:27:37 +0200

    first commit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = require('./routes.js');
 
 const app = express()
-
+ 
 app.set('view engine', 'pug')
 app.use( router ); 
 app.use( express.static('public') );
