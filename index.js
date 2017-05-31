fff180a6481d1735944c5324d855b973ca09ea76
Tim Holwerda <tim@highvibes.io>
2017-05-31 16:25:44 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = require('./routes.js');
+
+const app = express()
+
+app.set('view engine', 'pug')
+app.use( router ); 
+app.use( express.static('public') );
+
+app.listen(3000, function () {
+  console.log('PWA listening on port 3000!')
+})
